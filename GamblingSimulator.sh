94e185714989f06a3e8e7e8f615eb204b099bd69
dbhide <dhanashree.bhide3@gmail.com>
2020-01-30 12:47:27 +0530

    ADDED: Function to calculate total amount won or lost in 20 days

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -4,6 +4,7 @@ echo "Welcome to Gambling Simulation Problem"
 #Constants
 STAKE=100
 BET=1
+NUMBER_OF_DAYS=20
 
 function computePercentage() {
    MAXIMUM_STAKE=$(( $STAKE+(50*$STAKE/100) ))
@@ -20,11 +21,26 @@ function calculativeGambler() {
 	do
 		if [[ $((RANDOM%2)) -eq 1 ]]
 		then
-   		amount=$(( $amount+$BET ))
+			amount=$(( $amount+$BET ))
 		else
-   		amount=$(( $amount-$BET ))
+			amount=$(( $amount-$BET ))
 		fi
 	done
+	resultAmount=$(( $amount - $STAKE ))
+	echo $resultAmount
 }
 
-calculativeGambler
+function obtainTotalAmount() {
+	for(( i=1; i<=NUMBER_OF_DAYS; i++ ))
+	do
+		totalAmount=$(( totalAmount + $(calculativeGambler) ))
+	done
+	if [[ $totalAmount -gt 0 ]]
+	then
+		echo "Total Amount Won = $totalAmount "
+	else
+		echo "Total Amount Lost = $totalAmount"
+	fi
+}
+
+obtainTotalAmount
